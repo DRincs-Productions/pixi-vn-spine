798bc14639453d0f4e4e22d64ceb16a26899f6d2
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2025-12-19 19:53:37 +0100

    feat: update newSpine function to omit boundsProvider from SpineFromOptions

--- src/utils/spine-utility.ts
+++ src/utils/spine-utility.ts
@@ -1,7 +1,7 @@
 import { Spine as CoreSpine, SpineFromOptions } from "@drincs/pixi-vn-spine/core";
 import { Spine } from "src/components";
 
-export function newSpine(options: SpineFromOptions): Spine {
+export function newSpine(options: Omit<SpineFromOptions, "boundsProvider">): Spine {
     const spineCore = CoreSpine.from(options);
     const { skeleton, parent, ...props } = spineCore;
     const spine = new Spine({
