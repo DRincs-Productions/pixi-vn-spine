b867c56293a3fa10742cc85a5d4905c8bf6d11b6
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2026-01-29 17:37:08 +0100

    feat: update package version to 0.1.3; add spine-pixi-v8 dependency and adjust external dependencies in build config

--- tsup.submodules.config.ts
+++ tsup.submodules.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
     minify: true,
     bundle: true,
     skipNodeModulesBundle: false,
-    external: ["@drincs/pixi-vn", "@drincs/pixi-vn/pixi.js", "pixi.js"],
+    external: ["pixi.js", "@esotericsoftware/spine-core"],
     noExternal: ["@esotericsoftware/spine-pixi-v8"],
     esbuildOptions(options) {
         options.alias = {
