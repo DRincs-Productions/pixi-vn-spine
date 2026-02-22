dbf63cefb3d16dcff737ec362db49bf9a0e7f0fa
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2026-02-22 16:56:39 +0100

    refactor: optimize track clearing logic in Game component for improved readability

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,8 @@ export * from "./components";
 
 Game.addOnPreContinue(() => {
     // Clear all tracks on continue, to avoid weird states when the player continues while an animation is still playing.
-    CompleteOnContinueTracks.tracks.forEach(({ spine, tracks }) => {
-        tracks.forEach((track) => {
-            spine.clearTrack(track);
-        });
+    CompleteOnContinueTracks.tracks.values().forEach(({ tracks, spine }) => {
+        tracks.forEach((t) => spine.clearTrack(t));
     });
     CompleteOnContinueTracks.tracks.clear();
 });
