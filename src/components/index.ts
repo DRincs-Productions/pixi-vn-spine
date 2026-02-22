a4b480acd1161ca534e273012ac7307ee5b1a336
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2026-02-22 12:00:41 +0100

    feat: implement track clearing on continue in Game component to prevent animation state issues

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,14 +1,1 @@
-import { Game } from "@drincs/pixi-vn";
-import { CompleteOnContinueTracks } from "../memory/CompleteOnContinueTracks";
-
 export { default as Spine } from "./Spine";
-
-Game.addOnPreContinue(() => {
-    // Clear all tracks on continue, to avoid weird states when the player continues while an animation is still playing.
-    CompleteOnContinueTracks.tracks.forEach(({ spine, tracks }) => {
-        tracks.forEach((track) => {
-            spine.clearTrack(track);
-        });
-    });
-    CompleteOnContinueTracks.tracks.clear();
-});
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
+import { Game } from "@drincs/pixi-vn";
+import { CompleteOnContinueTracks } from "./memory/CompleteOnContinueTracks";
+
 export * from "./components";
+
+Game.addOnPreContinue(() => {
+    // Clear all tracks on continue, to avoid weird states when the player continues while an animation is still playing.
+    CompleteOnContinueTracks.tracks.forEach(({ spine, tracks }) => {
+        tracks.forEach((track) => {
+            spine.clearTrack(track);
+        });
+    });
+    CompleteOnContinueTracks.tracks.clear();
+});
