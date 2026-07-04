6ee5345c41861007883d124a57161636a22d8425
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2026-07-04 17:11:27 +0200

    chore: update GitHub Actions workflow, TypeScript configuration, and add TypeDoc configuration

---
+++ playground/src/main.tsx
@@ -0,0 +1,33 @@
+import { Container, Game, canvas, sound } from "@drincs/pixi-vn";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+
+// Canvas setup with PIXI
+const body = document.body;
+if (!body) {
+  throw new Error("body element not found");
+}
+
+Game.init(body, {}).then(() => {
+  // Pixi.JS UI Layer
+  canvas.addLayer("ui", new Container());
+
+  // Sound setup
+  sound.addChannel("bgm", { background: true });
+  sound.addChannel("sfx");
+  sound.defaultChannelAlias = "sfx";
+
+  // React setup with ReactDOM
+  const root = document.getElementById("root");
+  if (!root) {
+    throw new Error("root element not found");
+  }
+
+  const htmlLayout = canvas.addHtmlLayer("ui", root);
+  if (!htmlLayout) {
+    throw new Error("htmlLayout not found");
+  }
+  const reactRoot = createRoot(htmlLayout);
+
+  reactRoot.render(<App />);
+});
